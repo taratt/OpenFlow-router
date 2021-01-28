1e14d880d8818c9d31de18b60bb14cdc2451f182
taratt <51206120+taratt@users.noreply.github.com>
2021-01-29 03:20:40 +0330

    Update mytopo.py

--- mytopo.py
+++ mytopo.py
@@ -17,15 +17,15 @@ class MyTopo( Topo ):
         Topo.__init__( self )
 
         # Add hosts and switches
-        host1 = self.addHost( 'h1' ,ip="10.0.1.100/24", defaultRoute ="10.0.1.1")
-        host2 = self.addHost( 'h2' ,ip="10.0.2.100/24", defaultRoute ="10.0.2.1")
-        host3 = self.addHost('h3',ip="10.0.3.100/24", defaultRoute ="10.0.3.1")
+        host1 = self.addHost( 'h1' ,ip="10.0.1.100/24", defaultRoute ="via s1-eth1")
+        host2 = self.addHost( 'h2' ,ip="10.0.2.100/24", defaultRoute ="via s1-eth2")
+        host3 = self.addHost( 'h3' ,ip="10.0.3.100/24", defaultRoute ="via s1-eth3")
         switch1= self.addSwitch( 's1' )
 
         # Add links
         self.addLink( host1, switch1 )
         self.addLink( host2, switch1)
         self.addLink(host3, switch1)
-       
+
 
 topos = { 'mytopo': ( lambda: MyTopo() ) }
