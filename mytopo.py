c350caa5127d566ed086548a89123028e5ec8533
taratt <51206120+taratt@users.noreply.github.com>
2021-01-29 02:53:35 +0330

    Update mytopo.py

--- mytopo.py
+++ mytopo.py
@@ -20,7 +20,7 @@ class MyTopo( Topo ):
         host1 = self.addHost( 'h1' ,ip="10.0.1.100/24", defaultRoute ="via 10.0.1.1")
         host2 = self.addHost( 'h2' ,ip="10.0.2.100/24", defaultRoute ="via 10.0.2.1")
         host3 = self.addHost('h3',ip="10.0.3.100/24", defaultRoute ="via 10.0.3.1")
-        switch1= self.addSwitch( 's1' )
+        switch1= self.addSwitch( 's4' )
 
         # Add links
         self.addLink( host1, switch1 )
